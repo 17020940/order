d79ec5a12496e65a57ed1d0caad83c91db7b4970
17020940 <ducnha99bn@gmail.com>
2021-02-17 20:48:42 +0700

    connect to bluetooth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,27 +8,41 @@ import { connectableObservableDescriptor } from 'rxjs/internal/observable/Connec
 })
 export class AppComponent {
   title = 'order';
-  latitude = 12;
+  latitude;
   longitude;
+  battery;
 
   getLocation() {
     let option = { enableHighAccuracy: true, }
     navigator.geolocation.getCurrentPosition(position => {
       console.log(position)
       this.latitude = position.coords.latitude;
+      this.longitude = position.coords.longitude;
     });
   }
 
   getBluetoothDevice() {
     let nav: any = navigator;
     nav.bluetooth.requestDevice({
-      acceptAllDevices: true
+      filters: [{services: ['battery_service']}]
     })
     .then(device => {
-      console.log(device);
-      device.gatt.connect()
+      return device.gatt.connect();
     })
-    .catch(error => { console.error(error); });
+    .then(server => {
+      console.log(server)
+      return server.getPrimaryService('battery_service');
+    })
+    .then(service => {
+      return service.getCharacteristic('battery_level');
+    })
+    .then(characteristic => {
+      return characteristic.readValue();
+    })
+    .then(value => {
+      this.battery = value.getUint8(0);
+    })
+    .catch()
 
   }
 }
