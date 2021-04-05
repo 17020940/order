ec382c91b198ca2c913e717b3890f2087caa3dd4
17020940 <ducnha99bn@gmail.com>
2021-04-05 23:43:48 +0700

    tab pos

---
+++ ble-test-peripheral-android-master/app/src/main/java/io/github/webbluetoothcg/bletestperipheral/MyAdapter.java
@@ -0,0 +1,21 @@
+package io.github.webbluetoothcg.bletestperipheral;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class MyApdater extends FragmentStatePagerAdapter {
+    public MyApdater(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
